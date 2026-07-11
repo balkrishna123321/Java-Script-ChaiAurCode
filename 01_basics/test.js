f23463ab8543c85bb6921e9e2e315f409eea57a6
Balkrishna Patidar <balkrishnpatidar24@gmail.com>
2026-07-11 15:55:39 +0530

    full name

--- 01_basics/test.js
+++ 01_basics/test.js
@@ -1,6 +1,1 @@
-/* test.js v/s test.txt: The difference is in the file extension, which tells the operating system, what kind of file it is. (The .js extension tells the browser or Node.js:
-"This file contains JavaScript code. Parse and execute it.")
-
-Node.js is a JavaScript runtime environment.: It allows you to run JavaScript outside the browser, mainly on your computer or a server. No browser needed. Node.js uses Google's V8 JavaScript Engine, the same engine used in Chrome.
-To exicute the code: go to view, then open terminal, write node -v(to check installed or not?),then write your file name(node test.js) */
-console.log("Balkrishna")
+console.log("Balkrishna patidar");
