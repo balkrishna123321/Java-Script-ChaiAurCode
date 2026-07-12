d3c1a9a63bba43df9e8a114f951701aee0aff340
Balkrishna Patidar <balkrishnpatidar24@gmail.com>
2026-07-12 13:33:24 +0530

    type conversion

---
+++ 01_basics/03_conversionOperations.js
@@ -0,0 +1,6 @@
+// Type Conversion means converting a value from one data type to another. Implicit Conversion (Automatic) – JavaScript converts values automatically.
+// Explicit Conversion (Manual) – You convert values yourself using functions like Number(), String(), and Boolean().
+
+let score = 99
+console.log(typeof score)
+console.log(typeof (score))
