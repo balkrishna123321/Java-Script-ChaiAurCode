410ad0d087bde4378408d1fc8584025549414601
Balkrishna Patidar <balkrishnpatidar24@gmail.com>
2026-07-12 14:02:47 +0530

    String to Number

--- 01_basics/03_conversionOperations.js
+++ 01_basics/03_conversionOperations.js
@@ -1,6 +1,31 @@
 // Type Conversion means converting a value from one data type to another. Implicit Conversion (Automatic) – JavaScript converts values automatically.
 // Explicit Conversion (Manual) – You convert values yourself using functions like Number(), String(), and Boolean().
 
-let score = 99
+let score = "99abc" //string
 console.log(typeof score)
 console.log(typeof (score))
+
+// convertion: string to number
+let valueInNumber = Number(score)
+console.log(typeof valueInNumber); //number
+console.log(valueInNumber); //NaN: not a number
+//Number(Score):33, Number(33abc):NaN, Number(null):0, Number(undefined):NaN, Number(true):1, Number(false):0, Number(""):0
+
+console.log(Boolean(1));      // true
+console.log(Boolean(0));      // false
+
+console.log(Boolean(""));     // false
+console.log(Boolean("Hello")); // true
+
+console.log(Boolean(null));   // false
+console.log(Boolean(undefined)); // false
+
+
+let age = 20;
+let ageString = String(age);
+console.log(ageString);         // 20
+console.log(typeof ageString);  // string
+
+console.log("1"+2); //12
+
+console.log("1" + 2 + 2+2+"3"); //"12"+2="122"+2="1222"+"3"="12223"
