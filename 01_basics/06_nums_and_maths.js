dc43f210d2371d481d5e233dd041e5aab8031726
Balkrishna Patidar <balkrishnpatidar24@gmail.com>
2026-07-13 20:47:22 +0530

    nums

--- 01_basics/06_nums_and_maths.js
+++ 01_basics/06_nums_and_maths.js
@@ -5,7 +5,6 @@ console.log(score); //400 autometically define its a number
 console.log(typeof score); //type: number
 
 
-
 // You can also create a number using the Number constructor(ecplicitly).
 const num1 = new Number(400)
 console.log(num1); //[Number: 400]  : specificaly cast it is a number
@@ -33,10 +32,52 @@ console.log(num3.toLocaleString());        // US locale (depends on system)
 console.log(num3.toLocaleString('en-US')); // 123,456,789 : Output (US locale)
 console.log(num3.toLocaleString('en-IN')); // 12,34,56,789 : Indian format
 // Number Properties
-console.log(Number.MAX_VALUE);
-console.log(Number.MIN_VALUE);
-console.log(Number.MAX_SAFE_INTEGER);
-console.log(Number.MIN_SAFE_INTEGER);
+console.log(Number.MAX_VALUE); //1.7976931348623157e+308
+console.log(Number.MIN_VALUE); // 5e-324
+console.log(Number.MAX_SAFE_INTEGER); //9007199254740991
+console.log(Number.MIN_SAFE_INTEGER); //-9007199254740991
 
-// Math Object: The Math object provides mathematical constants and functions.
-console.log(Math);
+
+//*********************Maths******************* */
+console.log("Maths");
+
+// from math library: Math Object: The Math object provides mathematical constants and functions.
+console.log(Math); // Object [Math] {}
+// Common Math Methods: 1) Math.abs(): Returns the absolute (positive) value.
+console.log(Math.abs(-10)); //10
+//2) Math.round(): Rounds to the nearest integer.
+console.log(Math.round(4.6)); //5
+console.log(Math.round(4.4)); //4
+
+// 3) Math.ceil(): Rounds up. 4) Math.floor(): Rounds down. 5) Math.trunc(): Removes the decimal part.
+console.log(Math.ceil(4.1)); //5
+console.log(Math.floor(4.9)); //4
+console.log(Math.trunc(4.95)); //4
+
+//6. Math.sqrt(): Returns the square root. 7) Math.pow(): Raises a number to a power.
+console.log(Math.sqrt(6025)); //77.62087348130012
+console.log(Math.pow(2, 3)); //8
+
+//8. Math.min() and Math.max()
+console.log(Math.min(2, 4, 1, 9)); //1
+console.log(Math.max(2, 4, 1, 9)); //9
+//9. Math.random(): Returns a random number between 0 (inclusive) and 1 (exclusive).
+console.log(Math.random());
+
+
+// Random Number Between 1 and 10
+const random = Math.floor(Math.random() * 10) + 1;
+console.log(random);
+
+// Random Number Between Any Range: Example: 10 to 20
+const min = 10;
+const max = 20;
+const randomNum = Math.floor(Math.random() * (max - min + 1)) + min;
+console.log(randomNum);
+
+// Formula: Math.floor(Math.random() * (max - min + 1)) + min  :  This generates a random integer between min and max (both inclusive).
+
+//Useful Math Constants
+console.log(Math.PI); //3.141592653589793
+console.log(Math.E);  //2.718281828459045
+console.log(Math.SQRT2);  //1.4142135623730951
